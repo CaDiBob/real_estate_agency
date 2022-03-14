523be77a87bce99b8b14198153d9f27cd002efbc
CaDiBob <93527781+CaDiBob@users.noreply.github.com>
2022-03-14 22:13:07 +0900

    Добавил фильтры в админку

--- property/admin.py
+++ property/admin.py
@@ -10,6 +10,9 @@ class FlatAdmin(admin.ModelAdmin):
         'address', 'price', 'new_building', 'construction_year', 'town',
     )
     list_editable = ['new_building']
+    list_filter = [
+        'new_building', 'has_balcony', 'rooms_number',
+    ]
 
 
 admin.site.register(Flat, FlatAdmin)
